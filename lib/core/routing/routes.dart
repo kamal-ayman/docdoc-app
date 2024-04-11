5d684d6fee4f591e1b98fae3ef27725c048563a5
Kamal Ayman <kamal.ayman522@gmail.com>
2024-04-11 17:36:59 +0200

    Revert "Merge pull request #2 from kamal-ayman/development/features/onboarding"
    This reverts commit b89d1f231f4de473fe34ba1d3d7bde5738c4e5a6, reversing
    changes made to a8a5dde213067e95b75feabedfd604a2d887d34b.

--- lib/core/routing/routes.dart
+++
@@ -1,4 +0,0 @@
-class Routes {
-  static const String onBoardingScreen = '/onBoardingScreen';
-  static const String loginScreen = '/loginScreen';
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
-import 'package:docdoc_app/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 
-import 'doc_app.dart';
-
 void main() {
-  runApp(DocApp(appRouter: AppRouter()));
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
+
