4d4e179bbe9880fcece691430d04c916d2aea287
Kamal Ayman <kamal.ayman522@gmail.com>
2024-04-13 00:41:40 +0200

    - added flavors with github actions and firebase distribution.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 //
-// import 'package:docdoc_app/main.dart';
+// import 'package:docdoc_app/main_dev.dart';
 //
 // void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
