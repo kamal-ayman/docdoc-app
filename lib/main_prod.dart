b7467c401ceb9feccdbd5cbe2b459cc379bda149
Kamal Ayman <kamal.ayman522@gmail.com>
2024-04-13 00:41:40 +0200

    - added flavors with github actions and firebase distribution.

---
+++ lib/main_prod.dart
@@ -0,0 +1,13 @@
+import 'package:docdoc_app/core/di/dependency_injection.dart';
+import 'package:docdoc_app/core/routing/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'doc_app.dart';
+
+Future<void> main() async {
+  setupGitIt();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
+  runApp(DocApp(appRouter: AppRouter()));
+}
